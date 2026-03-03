697bfce228aea716d7a17ffb44ce76e29964ca66
MarkeyBass <MarkeyBass@gmail.com>
2026-03-03 22:33:10 +0200

    added more sort algorithms

--- 8-quick-sort/1-quick.js
+++ 8-quick-sort/1-quick.js
@@ -2,18 +2,20 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
-  let pivot = start;
+  let pivot = arr[start];
+  let swapIdx = start;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[pivot] > arr[i]) {
-      pivot++;
-      swap(arr, arr[pivot], arr[pivot + 1]);
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
     }
   }
+  swap(arr, swapIdx, start)
 
   console.log(arr);
-  console.log(pivot)
-  return pivot
+  console.log(pivot);
+  return pivot;
 }
 
 pivot([4, 8, 2, 1, 5, 7, 6, 3]);
