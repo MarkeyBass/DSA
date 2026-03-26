265b32766c23606fcea6fa34f3664da52d7b3dfc
MarkeyBass <MarkeyBass@gmail.com>
2026-03-26 06:03:47 +0200

    start adding Min Binary Heap

--- 14-binary-heaps/3-min-binary-heap.js
+++ 14-binary-heaps/3-min-binary-heap.js
@@ -15,7 +15,7 @@ class MinBinaryHeap {
     while (idx > 0) {
       let parentIdx = Math.floor((idx - 1) / 2);
       let parent = this.values[parentIdx];
-      if (element <= parent) break;
+      if (element >= parent) break;
       this.values[parentIdx] = element;
       this.values[idx] = parent;
       idx = parentIdx;
