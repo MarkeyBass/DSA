223ee19d2b5159faa18730ae94b66517deb9690e
MarkeyBass <MarkeyBass@gmail.com>
2026-03-29 02:05:32 +0300

    remove vertex

--- 16-graphs/graph.js
+++ 16-graphs/graph.js
@@ -4,7 +4,7 @@ class Graph {
   }
 
   #edgesErrorHandler(v1, v2) {
-    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
+    if (!this.adjacencyList[v1] || (v2 && !this.adjacencyList[v2])) {
       throw new Error("Vertex not found");
     }
   }
@@ -15,6 +15,7 @@ class Graph {
 
   addEdge(v1, v2) {
     this.#edgesErrorHandler(v1, v2);
+    if (this.adjacencyList[v1].includes(v2)) return;
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
@@ -30,6 +31,17 @@ class Graph {
     this.adjacencyList[v1] = this.adjacencyList[v1].filter((vertex) => vertex !== v2);
     this.adjacencyList[v2] = this.adjacencyList[v2].filter((vertex) => vertex !== v1);
   }
+
+  removeVertex(v) {
+    this.#edgesErrorHandler(v);
+
+    while (this.adjacencyList[v].length) {
+      const adjacentVertex = this.adjacencyList[v].pop();
+      this.removeEdge(v, adjacentVertex);
+    }
+
+    delete this.adjacencyList[v];
+  }
 }
 
 const g = new Graph();
@@ -42,5 +54,6 @@ g.addEdge("Tokyo", "Dalas");
 g.addEdge("Tokyo", "San Francisco");
 
 console.log(g);
-g.removeEdge("Tokyo", "San Francisco");
+// g.removeEdge("Tokyo", "San Francisco");
+g.removeVertex("Dalas");
 console.log(g);
