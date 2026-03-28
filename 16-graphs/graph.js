7b9a245c3b9742bb04d4ff83ddaf3cab797586d2
MarkeyBass <MarkeyBass@gmail.com>
2026-03-28 23:53:42 +0300

    addEdge

--- 16-graphs/graph.js
+++ 16-graphs/graph.js
@@ -1,16 +1,29 @@
 class Graph {
   constructor() {
-    this.ajacencyList = {};
+    this.adjacencyList = {};
   }
 
-  addVertext(key) {
-    if (!this.adjacencyList[vertex]) this.ajacencyList[key] = [];
+  addVertext(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
+      throw new Error("Vertex not found");
+    }
+
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
   }
 }
 
+const g = new Graph();
 
-const g = new Graph()
+g.addVertext("Tokyo");
+g.addVertext("San Francisco");
+g.addVertext("Dalas");
+
+g.addEdge("Tokyo", "Dalas");
 
 
-g.addVertext("Tokyo")
-g.addVertext("San Francisco")
+console.log(g)
