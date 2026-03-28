186c083d8b99fa1381ad069b0bf30f6e21226369
MarkeyBass <MarkeyBass@gmail.com>
2026-03-28 23:42:50 +0300

    added Graph class and addVertex method

---
+++ 16-graphs/graph.js
@@ -0,0 +1,16 @@
+class Graph {
+  constructor() {
+    this.ajacencyList = {};
+  }
+
+  addVertext(key) {
+    if (!this.adjacencyList[vertex]) this.ajacencyList[key] = [];
+  }
+}
+
+
+const g = new Graph()
+
+
+g.addVertext("Tokyo")
+g.addVertext("San Francisco")
