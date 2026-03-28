e69f2c6e1c8e6e2de0bdaa0e285f7e6bee089529
MarkeyBass <MarkeyBass@gmail.com>
2026-03-29 00:21:32 +0300

    remove edge

--- 16-graphs/graph.js
+++ 16-graphs/graph.js
@@ -3,20 +3,32 @@ class Graph {
     this.adjacencyList = {};
   }
 
+  #edgesErrorHandler(v1, v2) {
+    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
+      throw new Error("Vertex not found");
+    }
+  }
+
   addVertext(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
 
   addEdge(v1, v2) {
-    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
-      throw new Error("Vertex not found");
-    }
-    if (this.adjacencyList[v1].includes(v2) || this.adjacencyList[v2].includes(v1)) {
-      throw new Error("Edge already exists");
-    }
-
+    this.#edgesErrorHandler(v1, v2);
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.#edgesErrorHandler(v1, v2);
+    // let index = this.adjacencyList[v1].findIndex((el) => el === v2);
+    // this.adjacencyList[v1].splice(index, 1);
+
+    // index = this.adjacencyList[v2].findIndex((el) => el === v1);
+    // this.adjacencyList[v2].splice(index, 1);
+
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((vertex) => vertex !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((vertex) => vertex !== v1);
   }
 }
 
@@ -27,6 +39,8 @@ g.addVertext("San Francisco");
 g.addVertext("Dalas");
 
 g.addEdge("Tokyo", "Dalas");
+g.addEdge("Tokyo", "San Francisco");
 
-
-console.log(g)
+console.log(g);
+g.removeEdge("Tokyo", "San Francisco");
+console.log(g);
