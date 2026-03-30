5adbdc48aa05d87a92f7357b11a4049a337759ae
MarkeyBass <MarkeyBass@gmail.com>
2026-03-30 13:26:10 +0300

    added dfsIterative

--- Google-chalanges/sum-array-target.js
+++ Google-chalanges/sum-array-target.js
@@ -22,33 +22,17 @@ function checkSumSorted(arr, target = 8) {
 
 // For non sorted
 function checkSumNonSorted(arr, target) {
-  // Edge case: check if half of target exists twice inside the array
-  if (target % 2 === 0) {
-    let left = null;
-    let right = null;
-    const halfTarget = target / 2;
-    for (let el of arr) {
-      if (el === halfTarget) {
-        if (left === null) left = halfTarget;
-        else right = halfTarget;
-
-        if (left + right === target) return true;
-      }
-    }
-
-    // Main logic with Set
-    const set = new Set(arr);
-    for (let el of arr) {
-      if (set.has(target - el)) {
-        return true;
-      }
-    }
-
-    return false;
+  // // Main logic with Set
+  const set = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (set.has(target - arr[i])) return true;
+    set.add(arr[i]);
   }
+  return false;
 }
 
-console.log(checkSumNonSorted([1, 2, 3, 9],8)); // false
-console.log(checkSumNonSorted([1, 2, 4, 4],8)); // true 
-console.log(checkSumNonSorted([1, 2, 4, 5, 6],8)); // true
+console.log(checkSumNonSorted([1, 2, 3, 9], 8)); // false
+console.log(checkSumNonSorted([1, 2, 4, 4], 8)); // true
+console.log(checkSumNonSorted([1, 2, 4, 5, 6], 8)); // true
 console.log(checkSumNonSorted([1, 2, 4, 8, 9], 8)); // false
+console.log(checkSumNonSorted([1, 2, 4, 5, 9], 8)); // false
