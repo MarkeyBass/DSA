cfd81d6f940b99028d33f49fb09bf6a76c8d7f72
MarkeyBass <MarkeyBass@gmail.com>
2026-03-31 06:24:24 +0300

    added 1-d.js

--- 17-dijkstras/1-d.js
+++ 17-dijkstras/1-d.js
@@ -8,9 +8,8 @@ class WeightGraph {
      * */
   }
   addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList = [];
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
-
   addEdge(vertex1, vertex2, weight) {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
@@ -27,4 +26,4 @@ graph.addEdge("A", "B", 9);
 graph.addEdge("A", "C", 5);
 graph.addEdge("B", "C", 7);
 
-console.log(graph);
+console.log(graph.adjacencyList);
