473d6e833f7d1e0e85533822a2aff647237b3a81
MarkeyBass <MarkeyBass@gmail.com>
2026-04-02 17:56:01 +0300

    ittrative try

--- leetCodeMedum/3-Binary-Tree-Inorder-Traversal.js
+++ leetCodeMedum/3-Binary-Tree-Inorder-Traversal.js
@@ -61,29 +61,29 @@ function TreeNode(val, left, right) {
  */
 var inorderTraversal = function (root) {
   // // LIFO
-  // const stack = [];
   const results = [];
 
-  let currNode = root;
-  let currRoot = root;
+  // let currNode = root;
+  // let currRoot = root;
 
-  while (currNode) {
-    stack.push(currNode);
-    currRoot = currNode;
-    if (currRoot.left) {
-      while (currNode.left) {
-        stack.push(currNode.val);
-        currNode = currNode.left;
-      }
-    }
-    if (currNode.right) {
-      while (currRoot.right) {
-        stack.push(currNode.val);
-        currNode = currNode.right;
-      }
-    }
-    if(!currNode.left && !currNode.right) currRoot;
-  }
+  // while (currNode) {
+  //   stack.push(currNode);
+  //   currRoot = currNode;
+  //   if (currRoot.left) {
+  //     while (currNode.left) {
+  //       stack.push(currNode.val);
+  //       currNode = currNode.left;
+  //     }
+  //   }
+  //   if (currNode.right) {
+  //     while (currRoot.right) {
+  //       stack.push(currNode.val);
+  //       currNode = currNode.right;
+  //     }
+  //   }
+  //   if(!currNode.left && !currNode.right) currRoot;
+  // }
+  
   // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
   // Output: [4,2,6,5,7,1,3,9,8]
 
@@ -95,17 +95,72 @@ var inorderTraversal = function (root) {
 //            / \    /
 //           6   7  9
 
-    function traverse(root) {
-      if(!root) return;
-      if(root.left) traverse(root.left)
-        results.push(root.val)
-      if(root.right) traverse(root.right)
-    }
+    // function traverse(root) {
+    //   if(!root) return;
+    //   if(root.left) traverse(root.left)
+    //     results.push(root.val)
+    //   if(root.right) traverse(root.right)
+    // }
 
-    traverse(root)
+    // traverse(root)
+
+
+    let node = root
+    const stack = [];
+    while(node || stack.length){
+      if(node.left) {
+        stack.push(node)
+        node = node.left
+      } else {
+        results.push(node.val)
+        if(node.right) {
+          // stack.push(node.right)
+          node = node.right
+        }
+      }
+
+      if(!node.left && !node.right) {
+        node = stack.pop()
+      }
+
+    }
 
   return results;
 };
+
+
+// Call Stack
+// ============
+
+// Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+// Output: [4,2,6,5,7,1,3,9,8]
+
+//             1
+//            /  \
+//           2     3
+//          / \      \
+//         4   5      8
+//            / \    /
+//           6   7  9
+
+// traverse(1)
+// traverse(2)
+// traverse(4) push return [4]
+// traverse(2) push        [4, 2]
+// traverse(5)
+// traverse(6) push return [4, 2, 6]
+// traverse(5) push        [4, 2, 6, 5]
+// traverse(7) push return [4, 2, 6, 5, 7]
+// traverse(5)      return 
+// traverse(2)      return
+// traverse(1) push        [4, 2, 6, 5, 7, 1]
+// traverse(3) push        [4, 2, 6, 5, 7, 1, 3]
+// traverse(8) 
+// traverse(9) push return [4, 2, 6, 5, 7, 1, 3, 9]
+// traverse(8) push return [4, 2, 6, 5, 7, 1, 3, 9, 8]
+// traverse(3) return 
+// traverse(1) return
+
 
 const n1 = new TreeNode(1);
 const n2 = new TreeNode(2);
