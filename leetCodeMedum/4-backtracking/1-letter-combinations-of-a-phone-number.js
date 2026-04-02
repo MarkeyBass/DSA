a3e660b9267b6105f75285e448f724e44e5824be
MarkeyBass <MarkeyBass@gmail.com>
2026-04-02 20:34:25 +0300

    phone chars first commit

--- leetCodeMedum/4-backtracking/1-letter-combinations-of-a-phone-number.js
+++ leetCodeMedum/4-backtracking/1-letter-combinations-of-a-phone-number.js
@@ -19,13 +19,75 @@
 // 1 <= digits.length <= 4
 // digits[i] is a digit in the range ['2', '9'].
 
-// Letter Combinations of a Phone Number
+// Letter mapped to numbers
 // 2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"
 
 /**
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {
-    
+var letterCombinations = function (digits) {
+  if (typeof digits !== "string") {
+    digits = String(digits);
+  }
+
+  const LETTERS_OBJ = {
+    a: 2,
+    b: 2,
+    c: 2,
+    d: 3,
+    e: 3,
+    f: 3,
+    g: 4,
+    h: 4,
+    i: 4,
+    j: 5,
+    k: 5,
+    l: 5,
+    m: 6,
+    n: 6,
+    o: 6,
+    p: 7,
+    q: 7,
+    r: 7,
+    s: 7,
+    t: 8,
+    u: 8,
+    v: 8,
+    w: 9,
+    x: 9,
+    y: 9,
+    z: 9,
+  };
+  const NUMBERS_OBJ = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+
+  const combinations = [];
+
+  function inner(digits) {
+
+  }
+
+  
+  // for (let i = 0; i < digits.length; i++) {
+  //   const innerCombination = [];
+  //   for (let char of NUMBERS_OBJ[digits[i]]) {
+
+  //     innerCombination.push(char);
+  //   }
+  //   combinations.push(innerCombination);
+  // }
+
+  return combinations;
 };
+
+console.log(letterCombinations(23))
+
