2ac06c80083101e743448ac91547eeac9eb23952
MarkeyBass <MarkeyBass@gmail.com>
2026-04-05 02:19:37 +0300

    finished challange 2 - not 100% sure how

--- 18-dynamic-programing/challange-2.js
+++ 18-dynamic-programing/challange-2.js
@@ -5,24 +5,30 @@
 
 const denominations = [1, 5, 10, 25];
 
-coinChange(denominations, 1); // 1
-coinChange(denominations, 2); // 1
-coinChange(denominations, 5); // 2
-coinChange(denominations, 10); // 4
-coinChange(denominations, 25); // 13
-coinChange(denominations, 45); // 39
-coinChange(denominations, 100); // 242
-coinChange(denominations, 145); // 622
-coinChange(denominations, 1451); // 425663
-coinChange(denominations, 14511); // 409222339
+// coinChange(denominations, 1); // 1
+// coinChange(denominations, 2); // 1
+// coinChange(denominations, 5); // 2
+// coinChange(denominations, 10); // 4
+// coinChange(denominations, 25); // 13
+// coinChange(denominations, 45); // 39
+// coinChange(denominations, 100); // 242
+// coinChange(denominations, 145); // 622
+// coinChange(denominations, 1451); // 425663
+// coinChange(denominations, 14511); // 409222339
+
+
+coinChange(denominations, 6); // 2
 
 function coinChange(coins, amount) {
   const ways = new Array(amount + 1).fill(0);
   ways[0] = 1;
+  // outer loop over single coin
   for (const coin of coins) {
+    // For sums a from coin to amount, add ways to form a - coin (append this coin).
     for (let a = coin; a <= amount; a++) {
       ways[a] += ways[a - coin];
     }
+    console.log(ways)
   }
   console.log(ways[amount]);
   return ways[amount];
